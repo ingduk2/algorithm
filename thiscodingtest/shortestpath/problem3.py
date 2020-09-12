5560847f4c65fd1c904065d65b1e8b6170930593
ingduk2 <ingduk2@naver.com>
2020-09-13 03:57:39 +0900

    thiscodingtest-shortestpath

---
+++ thiscodingtest/shortestpath/problem3.py
@@ -0,0 +1,42 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+
+n, m, c = map(int, input().split())
+start = c
+
+graph = [[] for i in range(n+1)]
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+count = 0
+time = 0
+for i in range(1, n+1):
+    if distance[i] != INF:
+        count += 1
+        time = max(distance[i], time)
+
+print(count - 1, time)
+
